8c5bd996ec17f6eb04640aeafe3b9b38975bacea
Abdooorl <badamosiabdullahi@gmail.com>
2025-08-15 16:37:38 +0100

    Updated Services

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,12 +1,52 @@
 "use client";
 import Triangle from "@/../public/Triangle.png";
 import Image from "next/image";
+import { AudioLinesIcon } from "./ui/audio-lines";
+import { CastIcon } from "./ui/cast";
+import { SunIcon } from "./ui/sun";
+import { PartyPopperIcon } from "./ui/party-popper";
+import { ClapIcon } from "./ui/clap";
+import { TelescopeIcon } from "./ui/telescope";
+import { JSX } from "react";
+
 
 export default function Services() {
+  const servicesData : { icon: JSX.Element; title: string; description: string; }[] = [
+    {
+      icon: <AudioLinesIcon />,
+      title: "Sound & PA Systems",
+      description: "Crystal-clear audio for any event.",
+    },
+    {
+      icon: <CastIcon />,
+      title: "LED & Projector Screens",
+      description: "Vibrant visuals to captivate your audience.",
+    },
+    {
+      icon: <SunIcon />,
+      title: "Lighting Creation",
+      description: "Set the perfect mood with our lighting solutions.",
+    },
+    {
+      icon: <PartyPopperIcon />, 
+      title: "Event Decoration",
+      description: "Transform your space into something extraordinary.",
+    },
+    {
+      icon: <TelescopeIcon />,
+      title: "Exhibition Booths",
+      description: "Draw attention with our stunning booth designs.",
+    },
+    {
+      icon: <ClapIcon />,
+      title: "Stage & Stage Design",
+      description:   "Make your event unforgettable with our stage solutions.",
+    },
+  ];
   return (
     <div className="services  sticky w-full flex flex-col sm:flex-row-reverse   bg-white">
-      <div className="service-header relative flex min-h-[700px] sm:w-[50%] w-full  bg-[#714A97] ">
-        <div className="text w-full px-[18px] sm:px-[32px] sm:py-[32px] py-[28px] h-max flex flex-col gap-[4px]">
+      <div className="service-header relative sm:top-0  sm:sticky flex  min-h-[600px] sm:h-screen sm:w-[50%] w-full  bg-[#714A97] ">
+        <div className="text w-full px-[18px] sm:px-[32px] sm:py-[32px] py-[28px] h-max flex flex-col gap-[8px]">
           <h2 className="font-medium text-[40px] sm:text-[88px] text-white leading-[100%]">
             Our Services
           </h2>
@@ -29,7 +69,24 @@ export default function Services() {
           />
         </div>
       </div>
-      <div className="services sm:w-[50%]">hhhhb</div>
+      <div className="services sm:w-[50%]">
+        {servicesData.map((service, index) => (
+          <div
+            key={index}
+            className="service flex flex-col  gap-[20px] px-[18px] sm:h-[200px] h-[160px] justify-center sm:pl-[64px] sm:pr-[12px] border-b-[1px] border-[#ECECF2]"
+          >
+            <div className="icon cursor-pointer w-max">{service.icon}</div>
+            <div className="text flex flex-col gap-[4px]">
+              <h3 className="font-medium text-[28px] leading-[100%] sm:text-[40px] text-[#161616]">
+                {service.title}
+              </h3>
+              <p className="text-[16px] tracking-custom font-sans text-[#636363]">
+                {service.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
